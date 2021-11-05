a942116a4e37f7c57ca662e82d6e326a2a42115b
Juan Diego Bermeo <juandiego.bermeoortiz@uzh.ch>
2021-11-05 11:58:20 +0100

    Almost have it working
     - plots detected features and matches \n - implementes also OpenCVs SIFT for comparisson \n - Still detecting some wrong features :(

--- Exercise 4/sift.py
+++ Exercise 4/sift.py
@@ -1,10 +1,11 @@
 import os
-import typing
+from typing import Tuple
 
 import pandas as pd
 import numpy as np
 import cv2 as cv
 import scipy.ndimage as sn
+from scipy.spatial import distance
 import matplotlib.pyplot as plt
 
 
@@ -86,18 +87,19 @@ def get_dogs(blurred_imgs: np.ndarray) -> np.ndarray:
     return dog_array
 
 
-def generate_dog_pyramid(
+def generate_pyramid(
         input_img: np.ndarray,
         num_scales: int = 3,
         num_ocatves: int = 5,
         sigma: float = 1.6
-) -> list:
+) -> Tuple[list, list]:
     """
     Write code to compute the DoG pyramid by :
         1)  Computing 'num_scales + 3' blurred images per each octave.
         2)  Calculate DoGs using the blurred images. Each octave has 'num_scales + 2' DoGs
 
 
+    :type sigma: object
     :param input_img: Original image
     :param num_scales: Number of scales (scale factors for the gaussian kernels)
     :param num_ocatves: Number of pyramid levels (num_ocatves - 1 downsizings of the original image)
@@ -107,6 +109,7 @@ def generate_dog_pyramid(
     pyramid_input_img = input_img.copy()
 
     # Get DOG pyramid
+    blurred_imgs_pyramid = list()
     dog_pyramid = list()
     for octave_i in range(num_ocatves):
         if octave_i > 0:
@@ -116,33 +119,34 @@ def generate_dog_pyramid(
         # Get the Gaussians for each octave to compute DoG (returns 'num_scales + 3' blurred images)
         blurred_imgs_octave_i = get_blurred_images(input_img=pyramid_input_img.copy(), num_scales=num_scales,
                                                    sigma=sigma)
+        blurred_imgs_pyramid.append(blurred_imgs_octave_i)
 
         # Using the blurred images compute the DoG
         dogs_octave_i = get_dogs(blurred_imgs_octave_i)
         dog_pyramid.append(dogs_octave_i)
 
-    return dog_pyramid
+    return blurred_imgs_pyramid, dog_pyramid
 
 
-def locate_keypoints(dog_pyramid: list) -> pd.DataFrame:
+def locate_keypoints(dog_pyramid: list, min_threshold_c: float = 0.04) -> pd.DataFrame:
     """
     Compute the keypoints with non-maximum suppression and discard candidates with the contrast threshold.
 
+    :param min_threshold_c:
     :param dog_pyramid:
     :return: Dictionary with indexes to find each keypoint in the dog_pyramid (octave, scale, height, width)
     """
     key_point_location = {'octave': list(), 'scale': list(), 'height': list(), 'width': list()}
-    min_threshold_c = 0.04
 
     for octave_i in range(len(dog_pyramid)):
         # Set to 0 pixels below a threshold of C=0.04
         dog_octave_i = dog_pyramid[octave_i].copy()
-        dog_octave_i[dog_octave_i < min_threshold_c] = 0
 
         # Search for local maxima voxels in 26 (8 + 9 + 9) nearest neighbors.
         #   We do this via the dilation operation
-        local_max_values = sn.morphology.grey_dilation(input=dog_octave_i, size=(3, 9, 9))
-        local_maxima_idx = np.where(dog_octave_i[1: 4] == local_max_values[1: 4])
+        local_max_values = sn.morphology.grey_dilation(input=dog_octave_i, size=(3, 3, 3))
+        local_maxima_idx = np.where(
+            np.logical_and(dog_octave_i[1: 4] == local_max_values[1: 4], dog_octave_i[1: 4] >= min_threshold_c))
 
         key_point_location['octave'] += list(octave_i * np.ones_like(local_maxima_idx[0]))
         key_point_location['scale'] += list(local_maxima_idx[0])
@@ -152,35 +156,17 @@ def locate_keypoints(dog_pyramid: list) -> pd.DataFrame:
     return pd.DataFrame(key_point_location).set_index(['octave', 'scale'])
 
 
-def main(image_dir: str, num_scales: int = 3, num_ocatves: int = 5, sigma: float = 1.6):
-    """
-    Write code to compute:
-    % 1)    image pyramid. Number of images in the pyarmid equals
-    %       'num_octaves'.
-    % 2)    blurred images for each octave. Each octave contains
-    %       'num_scales + 3' blurred images.
-    % 3)    'num_scales + 2' difference of Gaussians for each octave.
-    % 4)    Compute the keypoints with non-maximum suppression and
-    %       discard candidates with the contrast threshold.
-    % 5)    Given the blurred images and keypoints, compute the
-    %       descriptors. Discard keypoints/descriptors that are too close
-    %       to the boundary of the image. Hence, you will most likely
-    %       lose some keypoints that you have computed earlier.
-    """
-    # Get paths to the images in image_dir
-    image_paths = tuple([os.path.join(image_dir, image_filename) for image_filename in os.listdir(image_dir)])
+def calculate_sift_descriptors(image, num_ocatves, num_scales, sigma):
+    blurred_imgs_pyramid, dog_pyramid_img = generate_pyramid(
+        image, num_scales=num_scales, num_ocatves=num_ocatves, sigma=sigma)
+    keypoints_img_df = locate_keypoints(dog_pyramid_img)
+    descriptor_df = get_descriptors(blurred_imgs_pyramid, keypoints_img_df)
 
-    img_1 = load_image(image_path=image_paths[0], re_scale_factor=0.2)
-    img_2 = load_image(image_path=image_paths[1], re_scale_factor=0.2)
-
-    dog_pyramid_img_1 = generate_dog_pyramid(img_1, num_scales=num_scales, num_ocatves=num_ocatves, sigma=sigma)
-    keypoints_img_1_df = locate_keypoints(dog_pyramid_img_1)
-
-    descriptor_df = get_descriptors(img_1, keypoints_img_1_df, num_scales, sigma)
-    print('hello')
+    return descriptor_df
 
 
-def get_descriptors(img: np.ndarray, keypoints_img_df: pd.DataFrame, num_scales: int, sigma: float) -> pd.DataFrame:
+def get_descriptors(blurred_imgs_pyramid: list, keypoints_img_df: pd.DataFrame) \
+        -> pd.DataFrame:
     descriptor_dict = {
         'octave': list(), 'scale': list(), 'height': list(), 'width': list(), 'descriptor': list()
     }
@@ -188,10 +174,8 @@ def get_descriptors(img: np.ndarray, keypoints_img_df: pd.DataFrame, num_scales:
     # Iterate the keypoints by grouping them first by the specific octave-scale combination we will use
     for (octave, scale), octave_scale_keypoints_df in keypoints_img_df.groupby(level=['octave', 'scale']):
         # Calculate new sigma to convolve the whole image
-        new_s = scale - 1 + num_scales * octave  # TODO: Verify if this is a plus or a minus
-        keypoint_sigma = np.power(2, new_s / num_scales) * sigma
-
-        blurred_img_o_s = use_gaussian_filter(input_image=img.copy(), sigma=keypoint_sigma)
+        # Access the blurred images from the pyramid
+        blurred_img_o_s = blurred_imgs_pyramid[octave][scale]
 
         g_dir, g_mag = get_gradient_mag_and_dir(blurred_img_o_s)
 
@@ -247,6 +231,137 @@ def get_descriptors(img: np.ndarray, keypoints_img_df: pd.DataFrame, num_scales:
             descriptor_dict['descriptor'].append(np.array(descriptor_patch_i).flatten())
 
     return pd.DataFrame(descriptor_dict).set_index(['octave', 'scale', 'height', 'width'])
+
+
+def match_features(img_1_descriptors_df: pd.DataFrame, img_2_descriptors_df: pd.DataFrame) -> pd.DataFrame:
+    # Match the features between images
+    # Calculate distances
+    # https://pypi.org/project/fastdist/
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html#scipy.spatial.distance.cdist
+    dist_matrix = distance.cdist(
+        XA=np.array(img_1_descriptors_df.descriptor.to_list()).astype(np.float16),
+        XB=np.array(img_2_descriptors_df.descriptor.to_list()).astype(np.float16)
+    )
+    # Get indices of closest described features
+    closest_two_features_df = pd.DataFrame(np.argpartition(dist_matrix, 2, axis=1)[:, :2],
+                                           columns=('closest_idx', 'second_closest_idx'))
+    # Get the value of these distances
+    closest_two_features_df['closest_feature_dist'] = \
+        np.take_along_axis(dist_matrix, closest_two_features_df.closest_idx.values.reshape((-1, 1)), axis=1)
+    closest_two_features_df['second_closest_feature_dist'] = \
+        np.take_along_axis(dist_matrix, closest_two_features_df.second_closest_idx.values.reshape((-1, 1)), axis=1)
+    # Calculate their ratio and filter those whose closest feature is not at least 0.8 smaller than the second
+    closest_two_features_df['dist_ratio'] = closest_two_features_df.closest_feature_dist / \
+                                            closest_two_features_df.second_closest_feature_dist
+    matched_features_df = closest_two_features_df[closest_two_features_df.dist_ratio < 0.8]
+    # Reshape the dataframe to return only the number (indices) of the matched features
+    matched_features_df = matched_features_df.reset_index()\
+            .loc[:, ['index', 'closest_idx', 'closest_feature_dist', 'dist_ratio']]
+
+    matched_features_df.columns = ['image_1_keypoint_idx', 'image_2_keypoint_idx', 'distance', 'distance_ratio']
+    matched_features_df.sort_values(['distance_ratio'], inplace=True)
+
+    return matched_features_df
+
+
+def convert_to_opencv_keypoints(img_1_sift_df: pd.DataFrame, img_2_sift_df: pd.DataFrame,
+                                matched_features_df: pd.DataFrame) -> Tuple[tuple, tuple, list]:
+    keypoints_img_1_cv = list()
+    keypoints_img_2_cv = list()
+    match_list = list()
+    for index, row in matched_features_df.reset_index().iterrows():
+        # Image 1 Keypoint
+        octave, scale, y, x = img_1_sift_df.iloc[int(row['image_1_keypoint_idx'])].name
+        keypoints_img_1_cv.append(cv.KeyPoint(x=float(x), y=float(y), size=float(scale), octave=octave))
+
+        # Image 2 Keypoint
+        octave, scale, y, x = img_2_sift_df.iloc[int(row['image_2_keypoint_idx'])].name
+        keypoints_img_2_cv.append(cv.KeyPoint(x=float(x), y=float(y), size=float(scale), octave=octave))
+
+        # Match object
+        match_list.append(cv.DMatch(_imgIdx=0, _queryIdx=index, _trainIdx=index,
+                                    _distance=row['distance']))
+
+    keypoints_img_1_cv = tuple(keypoints_img_1_cv)
+    keypoints_img_2_cv = tuple(keypoints_img_2_cv)
+
+    return keypoints_img_1_cv, keypoints_img_2_cv, match_list
+
+
+def main(image_dir: str, num_scales: int = 3, num_ocatves: int = 5, sigma: float = 1.6):
+    """
+    Write code to compute:
+    % 1)    image pyramid. Number of images in the pyarmid equals
+    %       'num_octaves'.
+    % 2)    blurred images for each octave. Each octave contains
+    %       'num_scales + 3' blurred images.
+    % 3)    'num_scales + 2' difference of Gaussians for each octave.
+    % 4)    Compute the keypoints with non-maximum suppression and
+    %       discard candidates with the contrast threshold.
+    % 5)    Given the blurred images and keypoints, compute the
+    %       descriptors. Discard keypoints/descriptors that are too close
+    %       to the boundary of the image. Hence, you will most likely
+    %       lose some keypoints that you have computed earlier.
+    """
+    # Get paths to the images in image_dir
+    image_paths = tuple([os.path.join(image_dir, image_filename) for image_filename in os.listdir(image_dir)])
+
+    img_1 = load_image(image_path=image_paths[0], re_scale_factor=0.3)
+    img_2 = load_image(image_path=image_paths[1], re_scale_factor=0.3)
+
+    img_1_sift_df = calculate_sift_descriptors(img_1, num_ocatves, num_scales, sigma)
+    img_2_sift_df = calculate_sift_descriptors(img_2, num_ocatves, num_scales, sigma)
+
+    matched_features_df = match_features(img_1_sift_df, img_2_sift_df)
+
+    # Plot the matched features using opencv's cv.drawMatches()
+    #   Let's re-use the cv.Keypoint class for this
+    keypoints_img_1_cv, keypoints_img_2_cv, custom_matches = convert_to_opencv_keypoints(
+        img_1_sift_df, img_2_sift_df, matched_features_df)
+
+    # Compare the features found with OpenCVs SIFT implementation and their matching function
+    img_1_keypoints = cv.drawKeypoints(img_1, keypoints_img_1_cv, img_1)
+    img_2_keypoints = cv.drawKeypoints(img_2, keypoints_img_2_cv, img_2)
+
+    sift = cv.SIFT_create()
+    kp_1, des_1 = sift.detectAndCompute(img_1, None)
+    kp_2, des_2 = sift.detectAndCompute(img_2, None)
+
+    img_1_keypoints_cv = cv.drawKeypoints(img_1, kp_1, img_1)
+    img_2_keypoints_cv = cv.drawKeypoints(img_2, kp_2, img_2)
+
+    # Compare the features
+    f, axarr = plt.subplots(2, 2)
+    f.tight_layout()
+    axarr[0, 0].imshow(img_1_keypoints)
+    axarr[0, 0].set_title('Self implemented SIFT')
+    axarr[0, 1].imshow(img_1_keypoints_cv)
+    axarr[0, 1].set_title("OpenCV's SIFT")
+
+    axarr[1, 0].imshow(img_2_keypoints)
+    axarr[1, 0].set_title('image 2', fontsize=8)
+    axarr[1, 1].imshow(img_2_keypoints_cv)
+    axarr[1, 1].set_title("image 2", fontsize=8)
+    plt.show()
+
+    # Compare the matches
+    f, axarr = plt.subplots(2, 1)
+    f.tight_layout()
+    bf = cv.BFMatcher(cv.NORM_L2, crossCheck=True)
+    matches = bf.match(des_1, des_2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    matched_features_opencv = cv.drawMatches(img_1, kp_1, img_2, kp_2, matches[:50], img_2, flags=2)
+    axarr[0].imshow(matched_features_opencv)
+    axarr[0].set_title("OpenCV's SIFT")
+
+    # Plot self implemented matches
+    matched_features_opencv = cv.drawMatches(img_1, keypoints_img_1_cv, img_2, keypoints_img_2_cv,
+                                             custom_matches[:50], img_2, flags=2)
+    axarr[1].imshow(matched_features_opencv)
+    axarr[1].set_title("Self implemented SIFT")
+    plt.show()
+
+    print('hello')
 
 
 if __name__ == '__main__':
